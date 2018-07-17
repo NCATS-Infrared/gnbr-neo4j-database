8d2ac15f2e184bacfacc5ca64f4d2ed29ae7403c
Stefano <srensi@stanford.edu>
2018-07-17 09:06:16 -0700

    shell script and sentences formattergi

--- download_gnbr.sh
+++ download_gnbr.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Set GNBR download url and folder
-BASE_URL="https://zenodo.org/record/1243969/files"
-DWNLD_DIR="$HOME/gnbr"
-NEO_DATA_DIR="$HOME/neo4j"
-SCRIPT_DIR=$(pwd)
-
 FILES=(\
 part-i-chemical-disease-path-theme-distributions.txt.gz \
 part-i-chemical-gene-path-theme-distributions.txt.gz \
@@ -16,12 +10,17 @@ part-ii-dependency-paths-gene-disease-sorted-with-themes.txt.gz \
 part-ii-dependency-paths-gene-gene-sorted-with-themes.txt.gz\
 )
 
+# Set GNBR download url and folder
+BASE_URL="https://zenodo.org/record/1243969/files"
+DWNLD_DIR="$HOME/gnbr"
+NEO_DATA_DIR="$HOME/neo4j"
 
-# mkdir -p $DWNLD_DIR $NEO_DATA_DIR
 
+mkdir -p $DWNLD_DIR $NEO_DATA_DIR
 for file in "${FILES[@]}";
 do echo $file;
-done | xargs -P 2 -n 1 -I foo echo ${BASE_URL}/foo -o ${DWNLD_DIR}/foo
+done | xargs -P 2 -n 1 -I foo curl ${BASE_URL}/foo -o ${DWNLD_DIR}/foo
 
-cd $START
+
 python -m format_gnbr ${DWNLD_DIR} ${NEO_DATA_DIR}
+
